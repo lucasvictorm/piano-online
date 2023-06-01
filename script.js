8d421e8abbf368cfd82e7520b2817a563e95595f
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-06-01 16:11:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ keys.forEach((key) => {
 
 showkeysbox.addEventListener('click', () => {showKeys()})
 
-const keysMap = {
+const keysDownMap = {
     "1":() => clickDown(keys[0]),
     "2":() => clickDown(keys[1]),
     "3":() => clickDown(keys[2]),
@@ -79,7 +79,38 @@ const keysMap = {
     "f":() => clickDown(keys[23])
 }
 
-document.body.addEventListener('keydown', (key) => {
+const keysUpMap = {
+    "1":() => clickUp(keys[0]),
+    "2":() => clickUp(keys[1]),
+    "3":() => clickUp(keys[2]),
+    "4":() => clickUp(keys[3]),
+    "5":() => clickUp(keys[4]),
+    "6":() => clickUp(keys[5]),
+    "7":() => clickUp(keys[6]),
+    "8":() => clickUp(keys[7]),
+    "9":() => clickUp(keys[8]),
+    "0":() => clickUp(keys[9]),
+    "q":() => clickUp(keys[10]),
+    "w":() => clickUp(keys[11]),
+    "e":() => clickUp(keys[12]),
+    "r":() => clickUp(keys[13]),
+    "t":() => clickUp(keys[14]),
+    "y":() => clickUp(keys[15]),
+    "u":() => clickUp(keys[16]),
+    "i":() => clickUp(keys[17]),
+    "o":() => clickUp(keys[18]),
+    "p":() => clickUp(keys[19]),
+    "a":() => clickUp(keys[20]),
+    "s":() => clickUp(keys[21]),
+    "d":() => clickUp(keys[22]),
+    "f":() => clickUp(keys[23])
+}
+
+document.body.addEventListener('keypress', (key) => {
     key.preventDefault();
-    keysMap[key.key]();
+    keysDownMap[key.key]();
+});
+document.body.addEventListener('keyup', (key) => {
+    key.preventDefault();
+    keysUpMap[key.key]();
 });
