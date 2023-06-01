d4cf076b4133eb45cc9b7b9d960439181cef2b0c
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-06-01 14:30:08 -0300

    adição de arquivos de áudio e estilo ao apertar teclas

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const keys = document.querySelectorAll(".tecla");
+
+
+function clickDown(key){
+    if(key.className.includes('preta')){
+        key.classList.add('preta-click');
+        return;
+    }
+    key.classList.add("branca-click");
+}
+
+function clickUp(key){
+    key.classList.remove("branca-click")
+    key.classList.remove('preta-click');
+}
+
 keys.forEach((key) => {
-    key.addEventListener('click', () => {console.log('clique')})
+    key.addEventListener('mousedown', () => {clickDown(key)})
+    key.addEventListener('mouseup', () => {clickUp(key)})
+
 })
